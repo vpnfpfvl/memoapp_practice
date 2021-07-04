c1f1171cbb796811d94773a1b45945f8ca2a1677
sui <k12140629vpnf@icloud.com>
2021-07-04 16:42:34 +0900

    メモタイトル等を調整

--- app.rb
+++ app.rb
@@ -11,20 +11,22 @@ enable :method_override
 
 # errorや404を捕捉するため 環境設定を変更。
 # ただし、productionだとsinatra reloaderは機能しなくなる
-set :environment => :production
+# set :environment => :production
 
 
 get '/' do
-  @title = "メモ帳"
+  @title = pagetitle('一覧')
+  @h1 = "メモノート"
   erb :index
 end
 
 get '/add_new_memo' do
+  @title = pagetitle('新規作成')
   erb :add_new_memo
 end
 
 post '/post_complete' do
-  @title = html_escape(params[:title])
+  @title = pagetitle(html_escape(params[:title]))
   @memo_text = html_escape(params[:memo_text])
   erb :post_complete
 end
@@ -33,12 +35,16 @@ get '/memo/:memo_uuid' do |memo_uuid|
   hash = {}
   File.open("data/#{memo_uuid}.json") do |f|
   hash = JSON.load(f)
-  end
-  @hash = hash
+end
+@hash = hash
+@title = pagetitle(hash["title"])
+
   erb :memo_detail
 end
 
-delete '/memo/:memo_uuid/delete_complete' do |memo_uuid|
+delete '/memo/:memo_uuid/delete_complete' do 
+  |memo_uuid|
+  @title = pagetitle('削除')
   @memo_uuid = memo_uuid
   erb :delete_complete
 end
@@ -51,6 +57,7 @@ patch '/memo/:memo_uuid/edit_complete' do |memo_uuid|
 end
 
 get '/memo/:memo_uuid/edit' do |memo_uuid|
+  @title = pagetitle('編集')
   hash = {}
   File.open("data/#{memo_uuid}.json") do |f|
   hash = JSON.load(f)
@@ -60,9 +67,15 @@ get '/memo/:memo_uuid/edit' do |memo_uuid|
 end
 
 not_found do
+  @title = pagetitle('ファイルが存在しません')
   'ファイルが存在しません'
 end
 
 error do
+  @title = pagetitle('エラー')
   'エラーが発生しました。 - ' + env['sinatra.error'].message
+end
+
+def pagetitle(title)
+  return "#{title} / メモ帳"
 end
