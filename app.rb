23959b0ba247de3a1a7d046c7d989349835564d8
sui <k12140629vpnf@icloud.com>
2021-07-06 15:24:30 +0900

    リファクタリング済み。rubocop残り一件

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
-require "erb"
+require 'erb'
 require 'securerandom'
 
-# htmlをエスケープするモジュールを読み込む
-include ERB::Util
-
 # put/deleteフォームを、それらをサポートしないブラウザで使えるように_methodのおまじないを使えるようにする。
-enable :method_override 
+enable :method_override
 
 # errorや404を捕捉するため 環境設定を変更。
 # ただし、productionだとsinatra reloaderは機能しなくなる
-# set :environment => :production
+set environment: :production
 
+# htmlをエスケープするモジュールを読み込む
+include ERB::Util
 
 get '/' do
   @title = pagetitle('一覧')
@@ -26,59 +27,39 @@ get '/add_new_memo' do
 end
 
 post '/post_complete' do
-  # title = html_escape(params[:title])
-  # memo_text = html_escape(params[:memo_text])
-
-  hash = {
-  memo_uuid: "#{SecureRandom.uuid}",
-  title: "#{html_escape(params[:title])}",
-  text: "#{html_escape(params[:memo_text])}"
-  }
-  hash[:title] = "タイトルなし" if hash[:title].empty? 
-
-  File.open("data/#{hash[:memo_uuid]}.json","w") {|file| 
-    file.puts(JSON.generate(hash))
-  }
-
+  hash = make_new_hash(
+    SecureRandom.uuid.to_s,
+    html_escape(params[:title]).to_s,
+    html_escape(params[:memo_text]).to_s
+  )
+  write_jsonfile(hash)
   redirect '/'
 end
 
 get '/memo/:memo_uuid' do |memo_uuid|
-  @hash = {}
-  File.open("data/#{memo_uuid}.json") do |f|
-    @hash = JSON.load(f)
-  end
-  @title = pagetitle(@hash["title"])
+  @hash = convert_jsonfile_to_hash("data/#{memo_uuid}.json")
+  @title = pagetitle(@hash['title'])
   erb :memo_detail
 end
 
 delete '/memo/:memo_uuid/delete_complete' do |memo_uuid|
   File.delete("data/#{memo_uuid}.json")
-  
   redirect '/'
 end
 
 patch '/memo/:memo_uuid/edit_complete' do |memo_uuid|
-  hash = {
-  memo_uuid: "#{memo_uuid}",
-  title: "#{html_escape(params[:title])}",
-  text: "#{html_escape(params[:memo_text])}"
-  }
-  hash[:title] = "タイトルなし" if hash[:title].empty?
-
-  File.open("data/#{hash[:memo_uuid]}.json","w") {|file| 
-    file.puts(JSON.generate(hash))
-  }
-
+  hash = make_new_hash(
+    memo_uuid.to_s,
+    html_escape(params[:title]).to_s,
+    html_escape(params[:memo_text]).to_s
+  )
+  write_jsonfile(hash)
   redirect "/memo/#{hash[:memo_uuid]}"
 end
 
 get '/memo/:memo_uuid/edit' do |memo_uuid|
   @title = pagetitle('編集')
-  @hash = {}
-  File.open("data/#{memo_uuid}.json") do |f|
-    @hash = JSON.load(f)
-  end
+  @hash = convert_jsonfile_to_hash("data/#{memo_uuid}.json")
   erb :memo_edit
 end
 
@@ -89,9 +70,26 @@ end
 
 error do
   @title = pagetitle('エラー')
-  'エラーが発生しました。 - ' + env['sinatra.error'].message
+  puts "エラーが発生しました。 -  #{env['sinatra.error'].message}"
 end
 
 def pagetitle(title)
-  return "#{title} / メモ帳"
+  "#{title} / メモ帳"
+end
+
+def convert_jsonfile_to_hash(source)
+  File.open(source) do |f|
+    JSON.parse(f.read)
+  end
+end
+
+def make_new_hash(uuid, title, text)
+  hash = { memo_uuid: uuid, title: title, text: text }
+  hash[:title] = 'タイトルなし' if hash[:title].empty?
+end
+
+def write_jsonfile(hash)
+  File.open("data/#{hash[:memo_uuid]}.json", 'w') do |file|
+    file.puts(JSON.generate(hash))
+  end
 end
